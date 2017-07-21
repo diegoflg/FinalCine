86686cbf0ee4d7fa398014ae52f4cc11ce6a6fc2
fercrisos <feryuncris@gmail.com>
2017-07-20 21:35:15 -0500

    ListadoAsiento

---
+++ Semana13_2_AJAX_Cine/Areas/Invitado/Controllers/AsientoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Semana13_2_AJAX_Cine.Areas.Invitado.Controllers
+{
+    public class AsientoController : Controller
+    {
+        // GET: Invitado/Asiento
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Listado()
+        {
+            return PartialView(DAGeneral.ListadoAsiento());
+        }
+    }
+}
