c01ef5ffaac14da44a5ad3b03442714bc1905a13
fercrisos <feryuncris@gmail.com>
2017-07-20 21:23:13 -0500

    Listado por tabla, Controler de protagonista

---
+++ Semana13_2_AJAX_Cine/Areas/Invitado/Controllers/ProtagonistaController.cs
@@ -0,0 +1,22 @@
+﻿using Semana13_2_AJAX_Cine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Semana13_2_AJAX_Cine.Areas.Invitado.Controllers
+{
+    public class ProtagonistaController : Controller
+    {
+        // GET: Invitado/Protagonista
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Listado()
+        {
+            return PartialView(DAGeneral.ListadoProtagonista());
+        }
+    }
+}
--- Semana13_2_AJAX_Cine/Models/DAGeneral.cs
+++ Semana13_2_AJAX_Cine/Models/DAGeneral.cs
@@ -15,5 +15,81 @@ namespace Semana13_2_AJAX_Cine.Models
                 return data.Interprete.ToList();
             }
         }
+
+        static public List<Protagonista> ListadoProtagonista()
+        {
+            List<Protagonista> protagonista = new List<Protagonista>();
+            using (var data = new CineDBEntities())
+            {
+                return data.Protagonista.ToList();
+            }
+        }
+
+        static public List<Asiento> ListadoAsiento()
+        {
+            List<Asiento> asiento = new List<Asiento>();
+            using (var data = new CineDBEntities())
+            {
+                return data.Asiento.ToList();
+            }
+        }
+
+        static public List<Cine> ListadoCine()
+        {
+            List<Cine> cine = new List<Cine>();
+            using (var data = new CineDBEntities())
+            {
+                return data.Cine.ToList();
+            }
+        }
+
+        static public List<CinePelicula> ListadoCinePelicula()
+        {
+            List<CinePelicula> cinepelicula = new List<CinePelicula>();
+            using (var data = new CineDBEntities())
+            {
+                return data.CinePelicula.ToList();
+            }
+        }
+
+        static public List<Director> ListadoDirector()
+        {
+            List<Director> director = new List<Director>();
+            using (var data = new CineDBEntities())
+            {
+                return data.Director.ToList();
+            }
+        }
+
+        static public List<Funcion> ListadoFuncion()
+        {
+            List<Funcion> funcion = new List<Funcion>();
+            using (var data = new CineDBEntities())
+            {
+                return = data.Funcion.ToList();
+            }
+            return funcion;
+        }
+
+        static public List<Pedido> ListadoPedido()
+        {
+            List<Pedido> pedido = new List<Pedido>();
+            using (var data = new CineDBEntities())
+            {
+                pedido = data.Pedido.ToList();
+            }
+            return pedido;
+        }
+
+        static public List<CinePelicula> ListadoCine()
+        {
+            List<CinePelicula> cinepelicula = new List<CinePelicula>();
+            using (var data = new CineDBEntities())
+            {
+                cinepelicula = data.CinePelicula.ToList();
+            }
+            return cinepelicula;
+        }
+
     }
 }
